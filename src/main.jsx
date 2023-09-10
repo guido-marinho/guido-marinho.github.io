820e92de9e8d6ccdbda7bc571c645a651780495a
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-09-10 12:26:31 -0300

    ➕ feat: adiciona react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
-  </BrowserRouter>,
+    </React.StrictMode>
+  </BrowserRouter>
 );
