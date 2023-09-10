15ab65b5eddf05c571296b6aa2278660988b009c
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-09-10 14:06:47 -0300

    💄 feat: finaliza css inicial do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import '../CSS/Header.css';
+import cover from '../images/cover.png';
+import profile from '../images/profile.png';
 
 export default function Header() {
   return (
     <>
-      <div>CAPA</div>
-      <div>
-        <div>div 1 - botão de follow e botão de mensagem  e foto de perfil</div>
-        <h1>Nome</h1>
-        <div>sobre mim</div>
-        <span>icones como no tt - joined por ex</span>
+      <div className='cover-page'>
+        {/* depois mudar para uma animação */}
+        <img src={ cover } alt="cover" />
+      </div>
+      <div className='profile-container'>
+        <div className='profile'>
+          <img src={ profile } alt="profile image" className='profile-image' />
+          <div className='profile-buttons'>
+            <button>menssage</button>
+            <button>follow</button>
+          </div>
+        </div>
+        <h1 className='profile-name'>Guilherme Gatti Marinho</h1>
+        <div className='profile-about'>
+          <p>Web Developer Full Stack</p>
+        </div>
+        <span  className='profile-icons'> icon1: Availabe -  icon2: links - icon3: birthday - icon4: joined</span>
       </div>
     </>
   );
