cc666e645b7925472996df3b6e708b711d618237
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-09-25 17:03:51 -0300

    ✨ feat: adiciona estilo a pagina de links

--- src/pages/Links.jsx
+++ src/pages/Links.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import FormEmail from '../components/FormEmail.jsx';
 
+import '../CSS/Links.css';
+
 import github from '../images/github.png';
 import linkedin from '../images/linkedin.png';
 import whatsapp from '../images/whatsapp.png';
@@ -8,17 +10,17 @@ import youtube from '../images/youtube.png';
 export default function Links() {
   return (
     <>
-      <nav>
+      <nav className='nav-links'>
         <a href="https://wa.me/+5535999423364" target="_blank" rel="noopener noreferrer">
           <img src={ whatsapp } alt="whatsapp-icon" />
         </a>
         <a href="https://github.com/guilhermegattimarinho" target="_blank" rel="noopener noreferrer">
-          <img src={ github } alt="whatsapp-icon" />
+          <img src={ github } alt="whatsapp-icon" className='github-img'/>
         </a>
         <a href="https://www.linkedin.com/in/guilhermegattimarinho/" target="_blank" rel="noopener noreferrer">
           <img src={ linkedin } alt="whatsapp-icon" />
         </a>
-        <a href="" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.youtube.com/channel/UCz1y3jdXPAIbqvDJgyZyG4w" target="_blank" rel="noopener noreferrer">
           <img src={ youtube  } alt="whatsapp-icon" />
         </a>
       </nav>
