c6b11c5102a2eadbb7c017d274ec302d154a23ea
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-09-26 17:55:26 -0300

    ✨ feat: implementa feed

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import '../CSS/Feed.css';
+
+import dataFeed from '../api/feed';
+
+import profile from '../images/profile.png';
 
 export default function Feed() {
   return (
-    <div>Feed</div>
+    <>
+      <ul className='ul-feed-container'>
+        {
+          dataFeed.map((item) => (
+            <li key={item.id} className='card-feed'>
+
+              <div className="profile-feed">
+                <img 
+                  src={profile} 
+                  alt="profile" className='profile-feed-img'
+                />
+                <p className='name-feed'>
+                  <strong>Gui</strong> ✨
+                </p>
+                •
+                <p className='date-feed'>
+                  {item.date}
+                </p>
+              </div>
+
+              <p className="feelings-feed">
+                {item.feeling}
+              </p>
+              <div className="post-feed">
+                <h2 className='title-feed'>
+                  {item.title}
+                </h2>
+
+                <p className='description-feed'>
+                  {item.description}
+                </p>
+
+                <img 
+                  src={ item.image} 
+                  alt={ item.title} 
+                  className='post-feed-img'
+                />
+              </div>
+              
+            </li>
+          ))
+        }
+      </ul>
+    </>
   );
 }
