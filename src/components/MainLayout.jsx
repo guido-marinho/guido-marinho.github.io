0a234eb2d6784ce32fc7d3f90c252a892a1cd0e9
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-09-25 22:17:02 -0300

    ✨ feat: adiciona pagina de about-me

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -3,6 +3,7 @@ import { Link, useLocation } from 'react-router-dom';
 
 import '../CSS/MainLayout.css';
 
+import About from '../pages/About.jsx';
 import FormContact from '../pages/FormContact.jsx';
 import Links from '../pages/Links.jsx';
 import Header from './Header.jsx';
@@ -10,7 +11,6 @@ import Header from './Header.jsx';
 
 export default function Main() {
   const path = useLocation().pathname;
-
 
   return (
     <>
@@ -27,16 +27,16 @@ export default function Main() {
               className={`h3-header-main ${path === '/' ? 'active' : ''}`}>
               Feed</h3>
           </Link>
-          <Link to='/projects' className='links-main'>
-            <h3 
-              className={`h3-header-main ${path === '/projects' ? 'active' : ''}`}>
-                Projetos
-            </h3>
-          </Link>
           <Link to='/about' className='links-main'>
             <h3 
               className={`h3-header-main ${path === '/about' ? 'active' : ''}`} >
                 Sobre mim
+            </h3>
+          </Link>
+          <Link to='/projects' className='links-main'>
+            <h3 
+              className={`h3-header-main ${path === '/projects' ? 'active' : ''}`}>
+                Projetos
             </h3>
           </Link>
           <Link to='/contact' className='links-main'>
@@ -49,8 +49,8 @@ export default function Main() {
         
         {path === '/links' && <Links />}
         {/* {path === '/' && <Feed />} */}
-        {/* {path === '/about' && <About />} */}
         {/* {path === '/projects' && <Projects />} */}
+        {path === '/about' && <About />}
         {path === '/contact' && <FormContact />}
       </main>
     </>
---
+++ src/pages/About.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <section className='about-container'>
+      <h2>Olá!</h2>
+      <br />
+      <p>
+        Eu sou <strong>Guilherme</strong> (mas pode me chamar de &quot;Gui&quot;). Sou um desenvolvedor web apaixonado em dar vida a projetos e ideias através de código.
+      </p>
+
+      <br />
+
+      <p>
+        Desde criança sempre fui facsinado por tecnologia e computadores. Em dezembro de 2022 resolvir dar um passo a mais e colocar toda essa paixão em prática, me matriculando no curso de Desenvolvimento Web Full Stack da Trybe.
+      </p>
+
+      <br />
+
+      <p>
+        Anteriormente, trilhei um percurso diversificado que me trouxe uma rica bagagem. De 2014 a 2019, trabalhei em um bar e restaurante, onde percorri uma trajetória ascendente até chegar ao cargo de gerente. Nessa função, tive a oportunidade de liderar equipes, gerenciar o caixa, controlar o estoque e administrar as redes sociais.
+
+        <br />
+
+        Nos anos de 2020 a 2022, embarquei em uma jornada empreendedora ao criar um curso de espanhol do absoluto zero. Este curso tinha como objetivo preparar brasileiros para a aprovação no exame SIELE, fundamental para ingressar em universidades de países de língua espanhola. Através dessa empreitada, pude aprimorar ainda mais minhas habilidades de comunicação, gestão de tempo, trabalho em equipe e adaptabilidade.
+      </p>
+
+      <br />
+
+      <p>
+        Hoje, como programador, aplico essa bagagem diversificada para abordar desafios de forma criativa e resolver problemas de maneira eficaz.
+      </p>
+
+      <br />
+
+      <p>
+        Se você tem um projeto ou uma ideia que gostaria de tirar do papel, entre em contato comigo! Vou adorar te ajudar a dar vida a esse sonho.
+      </p>
+
+      <br />
+
+      <p>
+        <strong>
+          Até breve!
+        </strong>
+      </p>
+    </section>
+  );
+}
