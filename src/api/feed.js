8e13ec52192974c4c021e81d8ddcc4a177401bd7
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-09-26 16:24:58 -0300

    ✨ feat:  cria fake api com db de feed

---
+++ src/api/feed.js
@@ -0,0 +1,12 @@
+const dataFeed = [
+  {
+    id: 1,
+    date: '2020-12-01',
+    title: 'Lorem ipsum dolor sit amet',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, arcu quis ornare facilisis, nisl leo maximus justo, eget ultrices quam nisi eget nisl. Mauris auctor, diam vel luctus commodo, nisl sem aliquam nunc, vitae aliquam elit massa ut odio. Donec vitae nunc euismod, aliquet nisi quis, aliquam arcu. Donec euismod, arcu quis ornare facilisis, nisl leo maximus justo, eget ultrices quam nisi eget nisl. Mauris auctor, diam vel luctus commodo, nisl sem aliquam nunc, vitae aliquam elit massa ut odio. Donec vitae nunc euismod, aliquet nisi quis, aliquam arcu.',
+    image: 'https://picsum.photos/200/300',
+  },
+];
+
+export default dataFeed;
