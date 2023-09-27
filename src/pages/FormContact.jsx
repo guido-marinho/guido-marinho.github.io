0115b0d63937bee26a99cc04e0614c141d6ae221
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-09-26 21:55:57 -0300

    ✨ feat: adiciona novas opções de contato

--- src/pages/FormContact.jsx
+++ src/pages/FormContact.jsx
@@ -1,8 +1,10 @@
 import emailjs from '@emailjs/browser';
 import React, { useState } from 'react';
+import Swal from 'sweetalert2';
 import '../CSS/FormContact.css';
 
-import Swal from 'sweetalert2';
+import linkedin from '../public/linkedin.png';
+import whatsapp from '../public/whatsapp.png';
 
 export default function FormEmail() {
   const [name, setName] = useState('');
@@ -57,45 +59,54 @@ export default function FormEmail() {
   };
 
   return (
-    <div className="form-container">
+    <div className='form-contact-container'>
+      <h3 className="title-contact">Por Whatsapp, Linkedin:</h3>
+      
+      <div className="contact-container">
+        <img src={ whatsapp} alt="whatsapp-icon" />
+        <img src={linkedin} alt="linkedin-icon" />
+      </div>
+      <h3 className="title-form"> ou E-Mail:</h3>
+      
+      <div className="form-container">
 
-
-      <form className="form-email" onSubmit={ handleSubmit }>
-        <label className="label-name" htmlFor="name">Nome <span className='required'>*</span></label>
-        <input 
-          className="input-name"
-          type="text"
-          placeholder="Digite seu nome"
-          onChange={({target: { value }}) => setName(value)}
-          value={ name }
-        />
+        <form className="form-email" onSubmit={ handleSubmit }>
+          <label className="label-name" htmlFor="name">Nome <span className='required'>*</span></label>
+          <input 
+            className="input-name"
+            type="text"
+            placeholder="Digite seu nome"
+            onChange={({target: { value }}) => setName(value)}
+            value={ name }
+          />
         
-        <label className="label-email" htmlFor="email">E-Mail <span className='required'>*</span></label>
-        <input 
-          className="input-email"
-          type="text"
-          placeholder="Digite seu email"
-          onChange={({target: { value }}) => setEmail(value)}
-          value={email}
-        />
+          <label className="label-email" htmlFor="email">E-Mail <span className='required'>*</span></label>
+          <input 
+            className="input-email"
+            type="text"
+            placeholder="Digite seu email"
+            onChange={({target: { value }}) => setEmail(value)}
+            value={email}
+          />
 
 
-        <label className="label-message" htmlFor="message">Mensagem <span className='required'>*</span></label>
-        <textarea 
-          className="textarea-message"
-          placeholder="Digite sua mensagem..."
-          onChange={({target: { value }}) => setMessage(value)}
-          value={message}
-        />
+          <label className="label-message" htmlFor="message">Mensagem <span className='required'>*</span></label>
+          <textarea 
+            className="textarea-message"
+            placeholder="Digite sua mensagem..."
+            onChange={({target: { value }}) => setMessage(value)}
+            value={message}
+          />
 
-        <input 
-          className="submit-email-btn"
-          type="submit" 
-          value="Enviar" 
-          disabled={ disabled() }
-        />
-      </form>
+          <input 
+            className="submit-email-btn"
+            type="submit" 
+            value="Enviar" 
+            disabled={ disabled() }
+          />
+        </form>
 
+      </div>
     </div>
   );
 }
