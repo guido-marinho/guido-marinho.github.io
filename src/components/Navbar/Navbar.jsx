ef1dd9bad3713b8d7fcd3d6d34a3dd6dfdd924d6
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-10-10 18:42:28 -0300

    🐛 fix: corrige nome de classes em navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,24 +9,24 @@ export default function Navbar()  {
     <nav className='nav-container'>
       <Link to='/' className='links-main'>
         <h3
-          className={`h3-header-main ${path === '/' ? 'active' : ''}`}>
+          className={`h3-nav ${path === '/' ? 'active' : ''}`}>
               Feed</h3>
       </Link>
       <Link to='/about' className='links-main'>
         <h3 
-          className={`h3-header-main ${path === '/about' ? 'active' : ''}`} >
+          className={`h3-nav ${path === '/about' ? 'active' : ''}`} >
                 Sobre mim
         </h3>
       </Link>
       <Link to='/projects' className='links-main'>
         <h3 
-          className={`h3-header-main ${path === '/projects' ? 'active' : ''}`}>
+          className={`h3-nav ${path === '/projects' ? 'active' : ''}`}>
                 Projetos
         </h3>
       </Link>
       <Link to='/contact' className='links-main'>
         <h3 
-          className={`h3-header-main ${path === '/contact' ? 'active' : ''}`}>
+          className={`h3-nav ${path === '/contact' ? 'active' : ''}`}>
               Contato
         </h3>
       </Link>
